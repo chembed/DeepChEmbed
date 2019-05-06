703a771cc4f8bda63a55494ac48e303f05620321
hanghu <hanghu@uw.edu>
2019-05-06 12:12:55 -0700

    revised __init__.py for subdirectory

--- deepchembed/notebook_scripts/__init__.py
+++ deepchembed/notebook_scripts/__init__.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-MODULE_PATH  = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-PACKAGE_PATH = os.path.join(MODULE_PATH, '..')
+MODULE_PATH  = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+PACKAGE_PATH = os.path.dirname(MODULE_PATH)
 DATA_PATH    = os.path.join(PACKAGE_PATH, 'data')
 
 if MODULE_PATH not in sys.path:
