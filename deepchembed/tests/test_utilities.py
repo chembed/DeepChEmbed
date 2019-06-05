c8e28b80af57a675cec4a031f10c785589b7dc87
yliu92uw <yliu92@uw.edu>
2019-06-04 20:05:16 -0700

    fix test function for cleaning out-of-bound label

--- deepchembed/tests/test_utilities.py
+++ deepchembed/tests/test_utilities.py
@@ -1,12 +1,14 @@
 import deepchembed.tests.__init__
 import utilities
+import pandas as pd
 
-def test_utilities():
+#def test_utilities():
 
-    return
+#    return
 
 def test_clean_out_of_bound():
-    
+    df = [-9,0,1,99,100,101]
+    assert utilities.clean_out_of_bound(df).equals(pd.Series([0,0,1,99,100,100]))
     return
 
 def test_bi_class():
