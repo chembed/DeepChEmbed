20868e24bd8b20bb9eefb16df2c77e550da531dd
yliu92uw <yliu92@uw.edu>
2019-06-04 19:42:28 -0700

    added functions

--- deepchembed/tests/test_utilities.py
+++ deepchembed/tests/test_utilities.py
@@ -4,3 +4,29 @@ import utilities
 def test_utilities():
 
     return
+
+def test_clean_out_of_bound():
+    
+    return
+
+def test_bi_class():
+
+    return
+
+def test_check_dtypes_count():
+
+    return
+
+def test_dedup_input_cols():
+
+    return
+
+def test_assign_class():
+    return
+
+def test_divide_classes():
+
+    return
+
+
+
